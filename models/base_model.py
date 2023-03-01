0f7d0c1b8867084540ea20d0b309f40e13e17797
Natha0b <laura.barona04@gmail.com>
2023-03-01 08:34:04 -0500

    add changes in the file base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""Import"""
 import uuid
 from datetime import datetime
 from models import storage
 
+
 class BaseModel:
+    """Define the class BaseModel"""
+
     def __init__(self, *args, **kwargs):
         if kwargs:
             for key, value in kwargs.items():
@@ -17,17 +21,19 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
-            
 
     def __str__(self):
+        """returns the string defined in this method."""
         return "[{}] ({}) {}".format(self.__class__.__name__,
-                                       self.id, self.__dict__)
-    
+                                     self.id, self.__dict__)
+
     def save(self):
+        """public instance attribute with the current datetime"""
         self.updated_at = datetime.now()
         storage.save()
-    
+
     def to_dict(self):
+        """returns a dictionary containing all keys/values"""
         dic_now = dict()
         for (key, value) in (self.__dict__).items():
             dic_now["__clas"] = self.__class__.__name__
