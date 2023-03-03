7178476467f0a5abbdce21125ecda3806cfab959
YuranyUlchur <ulchuryurani80@gmail.com>
2023-03-03 14:23:11 -0500

    add changes the comments

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -41,6 +41,8 @@ class FileStorage:
     '''Public instance methods'''
     def reload(self):
         '''deserializes the JSON file to __objects '''
+        from ..base_model import BaseModel
+        from ..user import User
         if path.exists(self.__file_path):
             with open(self.__file_path, mode='r') as file:
                 file_objects = json.load(file)
