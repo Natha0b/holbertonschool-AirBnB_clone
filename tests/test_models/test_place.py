4c3ceba071814553246c2143c24273852e0ebc6c
YuranyUlchur <ulchuryurani80@gmail.com>
2023-03-03 15:15:32 -0500

    add changes the commets

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -7,10 +7,29 @@ from models.place import Place
 class TestPlace(unittest.TestCase):
     '''test case the place'''
 
-    def test_place_creation(self):
-        '''check the attributes of a particular class'''
-        place = Place()
-        self.assertIsInstance(place, Place)
+    def setUp(self):
+        '''checks if an instance can be created'''
+        self.place = Place()
+
+    def test_instance_creation(self):
+        '''checks if the instance of the State
+        class has the required attributes.'''
+        self.assertIsInstance(self.place, Place)
+
+    def test_attributes(self):
+        '''checks if the name attribute
+        is correctly set to an empty string.'''
+        self.assertEqual(self.place.city_id, str())
+        self.assertEqual(self.place.user_id, str())
+        self.assertEqual(self.place.name, str())
+        self.assertEqual(self.place.description, str())
+        self.assertEqual(self.place.number_rooms, 0)
+        self.assertEqual(self.place.number_bathrooms, 0)
+        self.assertEqual(self.place.max_guest, 0)
+        self.assertEqual(self.place.price_by_night, 0)
+        self.assertEqual(self.place.latitude, 0.0)
+        self.assertEqual(self.place.longitude, 0.0)
+        self.assertEqual(self.place.amenity_ids, [])
 
 
 if __name__ == '__main__':
